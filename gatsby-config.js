092e70679ca851ea39d844bb35477e70bd486dbb
Ben Schroeder <btpschroeder@gmail.com>
2021-07-29 20:41:07 -0700

    Remove gatsby-icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
